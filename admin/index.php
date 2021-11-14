355ffd0e8227301dfb42afb504a76c1052a4dc35
Kittu <70826024+noob-kittu@users.noreply.github.com>
2021-11-14 19:01:09 +0530

    done

---
+++ admin/index.php
@@ -0,0 +1,2 @@
+/* This Software is developed to help make forms on requirements of companies to help collect data from users and can store in excel and database format also.>
+    Copyright (C) 2021  Noob-Kittu */
